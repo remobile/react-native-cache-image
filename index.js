7874e27acd611cda34b95048d6b598c8828e366c
fov42550564 <42550564@qq.com>
2017-03-30 14:03:22 +0800

    eslint code

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
-var React = require('react');
-var ReactNative = require('react-native');
+const React = require('react');
+const ReactNative = require('react-native');
 
-var {
+const {
     View,
     Image,
     Text,
@@ -10,74 +10,73 @@ var {
     ActivityIndicator,
 } = ReactNative;
 
-var StorageMgr = require('./storageMgr.js');
-var fs = require('react-native-fs');
+const StorageMgr = require('./storageMgr.js');
+const fs = require('react-native-fs');
 
-
-/*list status change graph
+/* list status change graph
 *
 *STATUS_NONE->[STATUS_LOADING]
 *STATUS_LOADING->[STATUS_LOADED, STATUS_UNLOADED]
 *
 */
-var
-STATUS_NONE = 0,
-STATUS_LOADING = 1,
-STATUS_LOADED = 2,
-STATUS_UNLOADED = 3;
+const
+    STATUS_NONE = 0,
+    STATUS_LOADING = 1,
+    STATUS_LOADED = 2,
+    STATUS_UNLOADED = 3;
 
-var storageMgr = new StorageMgr();
+const storageMgr = new StorageMgr();
 
-var CacheImage = React.createClass({
-    getInitialState() {
+const CacheImage = React.createClass({
+    getInitialState () {
         return {
             status:STATUS_NONE,
-        }
+        };
     },
-    componentWillMount() {
-        const {url} = this.props;
-        this.setState({status:STATUS_LOADING}, ()=>{
-            storageMgr.checkImageSource(url).then((source)=>{
+    componentWillMount () {
+        const { url } = this.props;
+        this.setState({ status:STATUS_LOADING }, () => {
+            storageMgr.checkImageSource(url).then((source) => {
                 if (this.state && this.state.status === STATUS_LOADING) {
                     if (source) {
-                        this.setState({status:STATUS_LOADED, source});
+                        this.setState({ status:STATUS_LOADED, source });
                     } else {
-                        this.setState({status:STATUS_UNLOADED});
+                        this.setState({ status:STATUS_UNLOADED });
                     }
                 }
             });
         });
     },
-    componentWillUnmount() {
+    componentWillUnmount () {
         this.state.status = STATUS_NONE;
         this.reloading = false;
     },
-    componentWillReceiveProps(nextProps) {
-        const {url} = nextProps;
+    componentWillReceiveProps (nextProps) {
+        const { url } = nextProps;
         if (url !== this.props.url) {
             this.reloading = true;
-            storageMgr.checkImageSource(url).then((source)=>{
+            storageMgr.checkImageSource(url).then((source) => {
                 if (this.reloading) {
                     this.reloading = false;
                     if (source) {
-                        this.setState({status:STATUS_LOADED, source});
+                        this.setState({ status:STATUS_LOADED, source });
                     }
                 }
             });
         }
     },
-    renderLoading() {
+    renderLoading () {
         return (
             <Image
                 {...this.props}
-                style={[this.props.style, {justifyContent:'center', alignItems:'center'}]}
+                style={[this.props.style, { justifyContent:'center', alignItems:'center' }]}
                 source={this.props.defaultSource}
                 >
-                <ActivityIndicator style={styles.spinner} size="small"/>
+                <ActivityIndicator style={styles.spinner} size='small' />
             </Image>
         );
     },
-    renderLocalFile() {
+    renderLocalFile () {
         return (
             <Image
                 {...this.props}
@@ -87,8 +86,8 @@ var CacheImage = React.createClass({
             </Image>
         );
     },
-    render() {
-        const {status} = this.state;
+    render () {
+        const { status } = this.state;
         if (status === STATUS_LOADING) {
             return this.renderLoading();
         } else if (status === STATUS_LOADED) {
@@ -101,7 +100,7 @@ var CacheImage = React.createClass({
                     >
                     {this.props.children}
                 </Image>
-            )
+            );
         }
     },
 });
@@ -109,7 +108,7 @@ var CacheImage = React.createClass({
 CacheImage.clear = storageMgr.clear;
 module.exports = CacheImage;
 
-var styles = StyleSheet.create({
+const styles = StyleSheet.create({
     spinner: {
         alignItems: 'center',
         justifyContent: 'center',
--- storageMgr.js
+++ storageMgr.js
@@ -4,208 +4,208 @@
 */
 'use strict';
 
-var Sqlite = require('@remobile/react-native-sqlite');
-var fs = require('react-native-fs');
-var FileTransfer = require('@remobile/react-native-file-transfer');
-var md5 = require("./md5.js");
+const Sqlite = require('@remobile/react-native-sqlite');
+const fs = require('react-native-fs');
+const FileTransfer = require('@remobile/react-native-file-transfer');
+const md5 = require('./md5.js');
 
-const DB_NAME = "cache_image";
-const TABLE_CACHE_IMAGE = "cache_image";
-const TABLE_CACHE_STORAGE = "cache_storage";
+const DB_NAME = 'cache_image';
+const TABLE_CACHE_IMAGE = 'cache_image';
+const TABLE_CACHE_STORAGE = 'cache_storage';
 const CACHE_IMAGE_DIR = 'cacheImages';
-const CACHE_IMAGE_SIZE = 1024*1024*50;
+const CACHE_IMAGE_SIZE = 1024 * 1024 * 50;
 
-var syncImageSource = {};
-var db = Sqlite.openDatabase({name:DB_NAME, location: 'default'});
+const syncImageSource = {};
+const db = Sqlite.openDatabase({ name:DB_NAME, location: 'default' });
 
 class StorageMgr {
-    constructor() {
-        var self = this;
+    constructor () {
+        const self = this;
         self.storage = 0;
-        fs.mkdir(fs.DocumentDirectoryPath+'/'+CACHE_IMAGE_DIR);
-        //console.log(fs.DocumentDirectoryPath+'/'+CACHE_IMAGE_DIR);
-        db.transaction((tx)=>{
-            tx.executeSql('CREATE TABLE IF NOT EXISTS '+TABLE_CACHE_IMAGE+' (filename varchar(40) primary key, size integer, time integer)');
-            tx.executeSql('CREATE TABLE IF NOT EXISTS '+TABLE_CACHE_STORAGE+' (key integer primary key, storage integer)');
-            tx.executeSql('SELECT storage FROM '+TABLE_CACHE_STORAGE+' WHERE key=1', [], (tx, rs)=>{
+        fs.mkdir(fs.DocumentDirectoryPath + '/' + CACHE_IMAGE_DIR);
+        // console.log(fs.DocumentDirectoryPath+'/'+CACHE_IMAGE_DIR);
+        db.transaction((tx) => {
+            tx.executeSql('CREATE TABLE IF NOT EXISTS ' + TABLE_CACHE_IMAGE + ' (filename constchar(40) primary key, size integer, time integer)');
+            tx.executeSql('CREATE TABLE IF NOT EXISTS ' + TABLE_CACHE_STORAGE + ' (key integer primary key, storage integer)');
+            tx.executeSql('SELECT storage FROM ' + TABLE_CACHE_STORAGE + ' WHERE key=1', [], (tx, rs) => {
                 if (rs.rows.length) {
                     self.storage = rs.rows.item(0).storage;
-                    //console.log('StorageMgr', self.storage);
+                    // console.log('StorageMgr', self.storage);
                 }
             });
-        }, (error)=>{
+        }, (error) => {
             console.log('StorageMgr <error>', error);
         });
     }
-    getCacheFilePath(filename) {
-        return fs.DocumentDirectoryPath+'/'+CACHE_IMAGE_DIR+'/'+filename+'.png';
+    getCacheFilePath (filename) {
+        return fs.DocumentDirectoryPath + '/' + CACHE_IMAGE_DIR + '/' + filename + '.png';
     }
-    lock(filename) {
+    lock (filename) {
         syncImageSource[filename] = true;
     }
-    unlock(filename) {
+    unlock (filename) {
         delete syncImageSource[filename];
     }
-    islock(filename) {
+    islock (filename) {
         return syncImageSource[filename];
     }
-    clear() {
-        var self = this;
-        fs.unlink(fs.DocumentDirectoryPath+'/'+CACHE_IMAGE_DIR);
-        db.transaction((tx)=>{
-            tx.executeSql('DROP TABLE '+TABLE_CACHE_IMAGE);
-            tx.executeSql('DROP TABLE '+TABLE_CACHE_STORAGE);
+    clear () {
+        const self = this;
+        fs.unlink(fs.DocumentDirectoryPath + '/' + CACHE_IMAGE_DIR);
+        db.transaction((tx) => {
+            tx.executeSql('DROP TABLE ' + TABLE_CACHE_IMAGE);
+            tx.executeSql('DROP TABLE ' + TABLE_CACHE_STORAGE);
             self.storage = 0;
-            fs.mkdir(fs.DocumentDirectoryPath+'/'+CACHE_IMAGE_DIR);
-            //console.log(fs.DocumentDirectoryPath+'/'+CACHE_IMAGE_DIR);
-            db.transaction((tx)=>{
-                tx.executeSql('CREATE TABLE IF NOT EXISTS '+TABLE_CACHE_IMAGE+' (filename varchar(40) primary key, size integer, time integer)');
-                tx.executeSql('CREATE TABLE IF NOT EXISTS '+TABLE_CACHE_STORAGE+' (key integer primary key, storage integer)');
-                tx.executeSql('SELECT storage FROM '+TABLE_CACHE_STORAGE+' WHERE key=1', [], (tx, rs)=>{
+            fs.mkdir(fs.DocumentDirectoryPath + '/' + CACHE_IMAGE_DIR);
+            // console.log(fs.DocumentDirectoryPath+'/'+CACHE_IMAGE_DIR);
+            db.transaction((tx) => {
+                tx.executeSql('CREATE TABLE IF NOT EXISTS ' + TABLE_CACHE_IMAGE + ' (filename constchar(40) primary key, size integer, time integer)');
+                tx.executeSql('CREATE TABLE IF NOT EXISTS ' + TABLE_CACHE_STORAGE + ' (key integer primary key, storage integer)');
+                tx.executeSql('SELECT storage FROM ' + TABLE_CACHE_STORAGE + ' WHERE key=1', [], (tx, rs) => {
                     if (rs.rows.length) {
                         self.storage = rs.rows.item(0).storage;
-                        //console.log('StorageMgr', self.storage);
+                        // console.log('StorageMgr', self.storage);
                     }
                 });
-            }, (error)=>{
+            }, (error) => {
                 console.log('StorageMgr <error>', error);
             });
         });
     }
-    addCacheImage(filename, size) {
-		return new Promise((resolve, reject) => {
-            db.transaction((tx)=>{
-                tx.executeSql('INSERT INTO '+TABLE_CACHE_IMAGE+' (filename, size, time) VALUES (?, ?, ?)', [filename, size, parseInt(Date.now()/1000)], (tx, rs)=>{
-                    //console.log('addCacheImage <insert>', filename, size);
+    addCacheImage (filename, size) {
+        return new Promise((resolve, reject) => {
+            db.transaction((tx) => {
+                tx.executeSql('INSERT INTO ' + TABLE_CACHE_IMAGE + ' (filename, size, time) VALUES (?, ?, ?)', [filename, size, parseInt(Date.now() / 1000)], (tx, rs) => {
+                    // console.log('addCacheImage <insert>', filename, size);
                     resolve(true);
                 });
-            }, (error)=>{
+            }, (error) => {
                 console.log('addCacheImage <error>', filename, size, error);
                 resolve(false);
             });
-		});
-	}
-    updateCacheImage(filename) {
-		return new Promise((resolve, reject) => {
-            db.transaction((tx)=>{
-                tx.executeSql('UPDATE '+TABLE_CACHE_IMAGE+' SET time=?'+' WHERE filename=?', [parseInt(Date.now()/1000), filename], (tx, rs)=>{
-                    //console.log('updateCacheImage <insert>', filename);
+        });
+    }
+    updateCacheImage (filename) {
+        return new Promise((resolve, reject) => {
+            db.transaction((tx) => {
+                tx.executeSql('UPDATE ' + TABLE_CACHE_IMAGE + ' SET time=?' + ' WHERE filename=?', [parseInt(Date.now() / 1000), filename], (tx, rs) => {
+                    // console.log('updateCacheImage <insert>', filename);
                     resolve(true);
                 });
-            }, (error)=>{
+            }, (error) => {
                 console.log('updateCacheImage <error>', filename, error);
                 resolve(false);
             });
-		});
-	}
-    deleteCacheImage() {
-        var self = this;
+        });
+    }
+    deleteCacheImage () {
+        const self = this;
         return new Promise((resolve, reject) => {
-            db.transaction((tx)=>{
-                tx.executeSql('SELECT filename, size FROM '+TABLE_CACHE_IMAGE+' WHERE time=(SELECT MIN(time) FROM '+TABLE_CACHE_IMAGE+')', [], (tx, rs)=>{
+            db.transaction((tx) => {
+                tx.executeSql('SELECT filename, size FROM ' + TABLE_CACHE_IMAGE + ' WHERE time=(SELECT MIN(time) FROM ' + TABLE_CACHE_IMAGE + ')', [], (tx, rs) => {
                     if (rs.rows.length) {
-                        var {filename, size} = rs.rows.item(0);
-                        tx.executeSql('DELETE FROM '+TABLE_CACHE_IMAGE+' WHERE filename=?', [filename], async (tx, rs)=>{
-                            //console.log('deleteCacheImage <delete>', filename, size);
+                        const { filename, size } = rs.rows.item(0);
+                        tx.executeSql('DELETE FROM ' + TABLE_CACHE_IMAGE + ' WHERE filename=?', [filename], async (tx, rs) => {
+                            // console.log('deleteCacheImage <delete>', filename, size);
                             await fs.unlink(self.getCacheFilePath(filename));
                             await self.updateStorage(-size);
                             resolve();
                         });
                     }
                 });
-            }, (error)=>{
+            }, (error) => {
                 console.log('deleteCacheImage <error>', error);
                 reject(error);
             });
         });
     }
-    updateStorage(offset) {
-        var self = this;
-        //console.log('StorageMgr updateStorage', self.storage, offset);
+    updateStorage (offset) {
+        const self = this;
+        // console.log('StorageMgr updateStorage', self.storage, offset);
         return new Promise(async(resolve, reject) => {
-            db.transaction((tx)=>{
-                tx.executeSql('UPDATE '+TABLE_CACHE_STORAGE+' SET storage=storage+?'+' WHERE key=1', [offset], (tx, rs)=>{
+            db.transaction((tx) => {
+                tx.executeSql('UPDATE ' + TABLE_CACHE_STORAGE + ' SET storage=storage+?' + ' WHERE key=1', [offset], (tx, rs) => {
                     if (rs.rowsAffected == 0) {
-                        tx.executeSql('INSERT INTO '+TABLE_CACHE_STORAGE+' (key, storage) VALUES (1, ?)', [offset], (tx, rs)=>{
-                            //console.log('updateStorage <insert>', offset);
+                        tx.executeSql('INSERT INTO ' + TABLE_CACHE_STORAGE + ' (key, storage) VALUES (1, ?)', [offset], (tx, rs) => {
+                            // console.log('updateStorage <insert>', offset);
                             self.storage = offset;
                             resolve(true);
                         });
                     } else {
-                        //console.log('updateStorage <update>', offset);
+                        // console.log('updateStorage <update>', offset);
                         self.storage += offset;
                         resolve(true);
                     }
                 });
-            }, (error)=>{
+            }, (error) => {
                 console.log('updateStorage <error>', error);
                 resolve(false);
             });
         });
     }
-    checkCacheStorage() {
-        var self = this;
+    checkCacheStorage () {
+        const self = this;
         return new Promise(async(resolve, reject) => {
-            //console.log('target:', self.storage);
+            // console.log('target:', self.storage);
             while (self.storage >= CACHE_IMAGE_SIZE) {
                 await self.deleteCacheImage();
-                //console.log('after:', self.storage);
+                // console.log('after:', self.storage);
             }
             resolve();
         });
     }
-    downloadImage(url, filepath, filename) {
-        var self = this;
+    downloadImage (url, filepath, filename) {
+        const self = this;
         return new Promise(async(resolve, reject) => {
-            var fileTransfer = new FileTransfer();
+            const fileTransfer = new FileTransfer();
             fileTransfer.download(
                 url,
                 filepath,
-                async(result)=>{
-                    console.log("downloadImage success:", result);
-                    var info = await fs.stat(filepath);
+                async(result) => {
+                    console.log('downloadImage success:', result);
+                    const info = await fs.stat(filepath);
                     await self.addCacheImage(filename, info.size);
                     await self.updateStorage(info.size);
                     await self.checkCacheStorage();
                     resolve(true);
                 },
-                (error)=>{
-                    console.log("downloadImage error:", error);
+                (error) => {
+                    console.log('downloadImage error:', error);
                     resolve(false);
                 },
                 true
             );
         });
     }
-    syncCheckImageSource(obj) {
-        var self = this;
+    syncCheckImageSource (obj) {
+        const self = this;
         if (self.islock(obj.filename)) {
-            setTimeout(()=>{self.syncCheckImageSource(obj)}, 100);
+            setTimeout(() => { self.syncCheckImageSource(obj); }, 100);
         } else {
             self.doCheckImageSource(obj);
         }
     }
-    async doCheckImageSource(obj) {
-        var {url, filename, filepath, resolve} = obj;
+    async doCheckImageSource (obj) {
+        const { url, filename, filepath, resolve } = obj;
         this.lock(filename);
-        var isExist = await fs.exists(filepath);
-        //console.log(this.param);
+        const isExist = await fs.exists(filepath);
+        // console.log(this.param);
         console.log('Is File exist', isExist);
         if (isExist) {
             await this.updateCacheImage(filename);
-            resolve({uri:'file://'+filepath});
+            resolve({ uri:'file://' + filepath });
         } else {
-            var success = await this.downloadImage(url, filepath, filename);
-            resolve(success ? {uri:'file://'+filepath} : null);
+            const success = await this.downloadImage(url, filepath, filename);
+            resolve(success ? { uri:'file://' + filepath } : null);
         }
         this.unlock(filename);
     }
-    checkImageSource(url) {
-        var self = this;
+    checkImageSource (url) {
+        const self = this;
         return new Promise(async(resolve, reject) => {
-            var filename =  md5(url);
-            var filepath = self.getCacheFilePath(filename);
-            self.syncCheckImageSource({url, filename, filepath, resolve});
+            const filename = md5(url);
+            const filepath = self.getCacheFilePath(filename);
+            self.syncCheckImageSource({ url, filename, filepath, resolve });
         });
     }
 }
