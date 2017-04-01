ad2ced00aaf8db317f835ece71d83389b0b48208
fov42550564 <42550564@qq.com>
2017-04-01 17:12:03 +0800

    发布1.0.3，解决点击快了本地图片不能显示出来的问题

--- index.js
+++ index.js
@@ -1,119 +1,81 @@
 'use strict';
 const React = require('react');
 const ReactNative = require('react-native');
-
 const {
-    View,
     Image,
-    Text,
-    StyleSheet,
-    ActivityIndicator,
 } = ReactNative;
 
+const TimerMixin = require('react-timer-mixin');
 const StorageMgr = require('./storageMgr.js');
-const fs = require('react-native-fs');
-
-/* list status change graph
-*
-*STATUS_NONE->[STATUS_LOADING]
-*STATUS_LOADING->[STATUS_LOADED, STATUS_UNLOADED]
-*
-*/
-const
-    STATUS_NONE = 0,
-    STATUS_LOADING = 1,
-    STATUS_LOADED = 2,
-    STATUS_UNLOADED = 3;
 
 const storageMgr = new StorageMgr();
 
 const CacheImage = React.createClass({
-    getInitialState () {
+    mixins: [TimerMixin],
+    getDefaultProps() {
         return {
-            status:STATUS_NONE,
+            timeOut: 60,
         };
     },
-    componentWillMount () {
-        const { url } = this.props;
-        this.setState({ status:STATUS_LOADING }, () => {
-            storageMgr.checkImageSource(url).then((source) => {
-                if (this.state && this.state.status === STATUS_LOADING) {
-                    if (source) {
-                        this.setState({ status:STATUS_LOADED, source });
-                    } else {
-                        this.setState({ status:STATUS_UNLOADED });
-                    }
+    getInitialState() {
+        return {
+            loaded: false,
+        }
+    },
+    componentWillMount() {
+        const {url} = this.props;
+        const time = Date.now();
+        storageMgr.checkImageSource(url).then((source)=>{
+            if (source) {
+                const diff = this.props.timeOut - (Date.now() - time);
+                if (diff > 0) {
+                    this.setTimeout(()=>{
+                        this.setState({loaded:true, source});
+                    }, diff);
+                } else {
+                    this.setState({loaded:true, source});
                 }
-            });
+            }
         });
     },
-    componentWillUnmount () {
-        this.state.status = STATUS_NONE;
+    componentWillUnmount() {
         this.reloading = false;
     },
-    componentWillReceiveProps (nextProps) {
-        const { url } = nextProps;
+    componentWillReceiveProps(nextProps) {
+        const {url} = nextProps;
         if (url !== this.props.url) {
             this.reloading = true;
-            storageMgr.checkImageSource(url).then((source) => {
+            const time = Date.now();
+            storageMgr.checkImageSource(url).then((source)=>{
                 if (this.reloading) {
                     this.reloading = false;
                     if (source) {
-                        this.setState({ status:STATUS_LOADED, source });
+                        const diff = this.props.timeOut - (Date.now() - time);
+                        if (diff > 0) {
+                            this.setTimeout(()=>{
+                                this.setState({loaded:true, source});
+                            }, diff);
+                        } else {
+                            this.setState({loaded:true, source});
+                        }
                     }
                 }
             });
         }
     },
-    renderLoading () {
+    render() {
+        const {loaded, source} = this.state;
+        const {defaultSource, children, ...other} = this.props;
         return (
             <Image
-                {...this.props}
-                style={[this.props.style, { justifyContent:'center', alignItems:'center' }]}
-                source={this.props.defaultSource}
-                >
-                <ActivityIndicator style={styles.spinner} size='small' />
-            </Image>
-        );
-    },
-    renderLocalFile () {
-        return (
-            <Image
-                {...this.props}
-                source={this.state.source}
+                {...other}
+                source={loaded ? source : defaultSource }
                 >
                 {this.props.children}
             </Image>
-        );
-    },
-    render () {
-        const { status } = this.state;
-        if (status === STATUS_LOADING) {
-            return this.renderLoading();
-        } else if (status === STATUS_LOADED) {
-            return this.renderLocalFile();
-        } else {
-            return (
-                <Image
-                    {...this.props}
-                    source={this.props.defaultSource}
-                    >
-                    {this.props.children}
-                </Image>
-            );
-        }
+        )
     },
 });
 
 CacheImage.clear = storageMgr.clear;
 module.exports = CacheImage;
-
-const styles = StyleSheet.create({
-    spinner: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: 25,
-        height: 25,
-        backgroundColor: 'transparent',
-    },
-});
